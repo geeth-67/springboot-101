1c5ba96406a55285b2b05e472c66f824e492891b
vishwageethanjana <geeth.me67@gmail.com>
2026-05-13 11:57:43 +0530

    start spring boot 13.05.2026

--- src/main/java/com/customer/demo/controller/CustomerController.java
+++ src/main/java/com/customer/demo/controller/CustomerController.java
@@ -1,10 +1,28 @@
 package com.customer.demo.controller;
 
+import com.customer.demo.models.Customer;
+import com.customer.demo.service.CustomerService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * @RestController is a combination of @Controller and @ResponseBody
+ */
 
 @RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
 
+    private final CustomerService customerService;
 
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    public List<Customer> getAllCustomer() {
+
+        return customerService.getAllCustomers();
+    }
 }
