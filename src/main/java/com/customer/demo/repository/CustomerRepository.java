97c7d23549c96cbabf8a6ff5091f54369b255bf0
vishwageethanjana <geeth.me67@gmail.com>
2026-05-13 10:32:40 +0530

    start spring boot 13.05.2026

---
+++ src/main/java/com/customer/demo/repository/CustomerRepository.java
@@ -0,0 +1,28 @@
+package com.customer.demo.repository;
+
+import com.customer.demo.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+/**
+ * JPA - Java persistence API
+ * <p>
+ * JPA repo gives below methods for free to use
+ * <p>
+ * save()<br>
+ * findById(id) = ORM generate this SQL - select * from customer where id = 001<br>
+ * findAll()<br>
+ * delete()<br>
+ * deleteById()<br>
+ * count()  AND some of them
+ */
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
