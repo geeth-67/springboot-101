ad91ffc9c8059bc5d7f8cba39c9851418245eb3d
vishwageethanjana <geeth.me67@gmail.com>
2026-05-13 11:13:18 +0530

    start spring boot 13.05.2026

--- src/main/java/com/customer/demo/models/Customer.java
+++ src/main/java/com/customer/demo/models/Customer.java
@@ -28,6 +28,57 @@ public class Customer {
     @Column(nullable = false)
     private String email;
 
-    @Column(name = "phone_number" , nullable = false)
+    @Column(name = "phone_number" , nullable = true , length = 14)
     private String phoneNo;
+
+
+    public Customer() {}
+
+    public Customer(String firstName , String lastName , String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
 }
