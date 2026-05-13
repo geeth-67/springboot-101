23be3a68cf06ccc41e3dde1e91e898d15d5513fd
vishwageethanjana <geeth.me67@gmail.com>
2026-05-13 11:13:15 +0530

    start spring boot 13.05.2026

---
+++ src/main/java/com/customer/demo/DTO/createCustomerRequest.java
@@ -0,0 +1,9 @@
+package com.customer.demo.DTO;
+
+public class createCustomerRequest {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private String email;
+}
